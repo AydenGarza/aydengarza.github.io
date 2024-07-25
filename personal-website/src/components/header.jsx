b2fee9a054edf59c85d76c7350d98101157cce36
Ayden Garza <aydenjgarza@gmail.com>
2024-07-25 18:51:07 -0500

    Ok I kinda lied in my last commit the highlighting in the nav bar was still broken but now its fixed... I think..

--- personal-website/src/components/header.jsx
+++ personal-website/src/components/header.jsx
@@ -5,20 +5,29 @@ import logo from '../pages/icons/site_logo.png'
 function Header( {className}){
     const location = useLocation();
 
+    function isActivePage(path){
+        if (path === '/home'){
+            if (location.pathname === '/home' || location.pathname === '/'){
+                return true;
+            }
+        }
+        return location.pathname === path;
+    }
+
     return<div className={PageHeaderStyling.PageHeader}>
         <div className={PageHeaderStyling.LogoContainer}><Link to="/home"><img src={logo} alt="Ayden Garza" className = {PageHeaderStyling.Logo}/></Link></div>
             <ul>
-                <Link to="/home" className={`${PageHeaderStyling.navLink} ${location.pathname === '/home' || '/' ? PageHeaderStyling.activeNavLink : ''}`}>
+                <Link to="/home" className={`${PageHeaderStyling.navLink} ${isActivePage('/home') ? PageHeaderStyling.activeNavLink : ''}`}>
                     <li>
                         Home
                     </li>
                 </Link>
-                <Link to="/about" className={`${PageHeaderStyling.navLink} ${location.pathname === '/about' ? PageHeaderStyling.activeNavLink : ''}`}>
+                <Link to="/about" className={`${PageHeaderStyling.navLink} ${isActivePage('/about') ? PageHeaderStyling.activeNavLink : ''}`}>
                     <li>
                         About
                     </li>
                 </Link>
-                <Link to="/projects" className={`${PageHeaderStyling.navLink} ${location.pathname === '/projects' ? PageHeaderStyling.activeNavLink : ''}`}>
+                <Link to="/projects" className={`${PageHeaderStyling.navLink} ${isActivePage('/projects') ? PageHeaderStyling.activeNavLink : ''}`}>
                     <li>
                         Projects
                     </li>
