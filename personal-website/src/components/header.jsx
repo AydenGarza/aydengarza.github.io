f826c41344de10dc1cf6f6af2bc093621d763f33
Ayden Garza <aydenjgarza@gmail.com>
2024-07-25 18:11:48 -0500

    Styled links, added active page indicator in navbar

--- personal-website/src/components/header.jsx
+++ personal-website/src/components/header.jsx
@@ -1,22 +1,24 @@
-import {Link} from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import PageHeaderStyling from './Header.module.css'
 import logo from '../pages/icons/site_logo.png'
 
 function Header( {className}){
+    const location = useLocation();
+
     return<div className={PageHeaderStyling.PageHeader}>
         <div className={PageHeaderStyling.LogoContainer}><Link to="/home"><img src={logo} alt="Ayden Garza" className = {PageHeaderStyling.Logo}/></Link></div>
             <ul>
-                <Link to="/home">
+                <Link to="/home" className={`${PageHeaderStyling.navLink} ${location.pathname === '/home' ? PageHeaderStyling.activeNavLink : ''}`}>
                     <li>
                         Home
                     </li>
                 </Link>
-                <Link to="/about">
+                <Link to="/about" className={`${PageHeaderStyling.navLink} ${location.pathname === '/about' ? PageHeaderStyling.activeNavLink : ''}`}>
                     <li>
                         About
                     </li>
                 </Link>
-                <Link to="/projects">
+                <Link to="/projects" className={`${PageHeaderStyling.navLink} ${location.pathname === '/projects' ? PageHeaderStyling.activeNavLink : ''}`}>
                     <li>
                         Projects
                     </li>
--- personal-website/src/pages/Home.jsx
+++ personal-website/src/pages/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
                     Hi, I'm Ayden.
                 </h1>
                 <main className={HomePageStyle.content}>
-                    Welcome to my site! I'm a computer science student at Colorado State University and a software engineering intern at OHEL Technologies. A <Link to="/about">tech nerd at heart</Link>, I love all things software, math, and machine learning. Some of my recent projects involve digital logic and legendary knights, <Link to="/projects">check them out</Link>!
+                    Welcome to my site! I'm a computer science student at Colorado State University and a software engineering intern at OHEL Technologies. A <Link to="/about" className={HomePageStyle.InParaLink}>tech nerd at heart</Link>, I love all things software, math, and machine learning. Some of my recent projects involve digital logic and legendary knights, <Link to="/projects" className={HomePageStyle.InParaLink}>check them out</Link>!
                 </main>
                 <ul className = {HomePageStyle.IconList}>
                     <li><a href="https://linkedin.com/in/ayden-garza" target="_blank" rel="noreferrer"><img src={linkedInIcon} alt="LinkedIn" className= {HomePageStyle.SocialIcon}/></a></li>
