f96fff47a9e008402d60c6fe7f16e37585ede1a3
Ayden Garza <aydenjgarza@gmail.com>
2024-07-25 15:16:33 -0500

    Added custom font, social media links, and a proper home paragraph

--- personal-website/src/pages/Home.jsx
+++ personal-website/src/pages/Home.jsx
@@ -1,13 +1,25 @@
 import Header from '../components/Header.jsx'
 import HomePageStyle from './Home.module.css'
+import linkedInIcon from './icons/linkedinicon.png'
+import githubIcon from './icons/ghicon.png'
+import emailIcon from './icons/emailicon.png'
 
 function Home() {
     return <>
         <Header className="header"></Header>
         <div className = {HomePageStyle.pageContainer}>
             <div className = {HomePageStyle.contentContainer}>
-                <h1 className = {HomePageStyle.greeting}> Hi! I'm Ayden.</h1>
-                <main className={HomePageStyle.content}>I'm a computer science student at Colorado State University and a software engineer intern at OHEL Technologies.</main>
+                <h1 className = {HomePageStyle.greeting}> 
+                    Hi, I'm Ayden.
+                </h1>
+                <main className={HomePageStyle.content}>
+                    Welcome to my site! I'm a computer science student at Colorado State University and a software engineering intern at OHEL Technologies. A <a href="/about">tech nerd at heart</a>, I love all things software, math, and machine learning. Some of my recent projects involve digital logic and legendary knights, <a href="/projects">check them out</a>!
+                </main>
+                <ul className = {HomePageStyle.IconList}>
+                    <li><a href="https://linkedin.com/in/ayden-garza" target="_blank" rel="noreferrer"><img src={linkedInIcon} alt="LinkedIn" className= {HomePageStyle.SocialIcon}/></a></li>
+                    <li><a href="https://github.com/AydenGarza" target="_blank" rel="noreferrer"><img src={githubIcon} alt="GitHub" className={HomePageStyle.SocialIcon}/></a></li>
+                    <li><a href="mailto:aydenjgarza@gmail.com" ><img src={emailIcon} alt="Email" className={HomePageStyle.SocialIcon}/></a></li>
+                </ul>
             </div>
         </div>
     </>
